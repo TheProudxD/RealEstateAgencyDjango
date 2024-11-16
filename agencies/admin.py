534fd1e4e6b89ee3636f758d6f85f41f989c82a9
proud <84202885+TheProudxD@users.noreply.github.com>
2024-11-16 16:27:41 +0300

    admin

--- agencies/admin.py
+++ agencies/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Agent, Agreement
 
-# Register your models here.
+class AgentAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'credit', 'agreement')
+    list_display_links = ('id', 'name')
+    search_fields = ('name', 'credit')
+    list_editable = ('agreement',)
+    list_filter = ('id', 'name', 'agreement')
+
+admin.site.register(Agreement)
+admin.site.register(Agent, AgentAdmin)
--- agencies/models.py
+++ agencies/models.py
@@ -8,6 +8,11 @@ class Agent(models.Model):
 
     def __str__(self):
         return self.name
+    
+    class Meta: 
+        verbose_name = 'Агент'
+        verbose_name_plural = 'Агенты'
+        ordering = ['id', 'name']
 
 
 class Client(models.Model):
@@ -19,22 +24,41 @@ class Client(models.Model):
 
     def get_absolute_url(self): 
         return reverse('client', kwargs={'cl_id': self.pk})
+    
+    class Meta: 
+        verbose_name = 'Клиент'
+        verbose_name_plural = 'Клиенты'
 
 
 class TypeOfPayment(models.Model):
     name = models.CharField(verbose_name="Наименование", max_length=30)
     agreement = models.ForeignKey('Agreement', on_delete=models.CASCADE, verbose_name='Договор', null=True)
 
+    class Meta: 
+        verbose_name = 'Агент'
+        verbose_name_plural = 'Агенты'
+
 
 class TypeOfEstate(models.Model):
     name = models.CharField(verbose_name="Наименование", max_length=30)
     agreement = models.ForeignKey('Agreement', on_delete=models.CASCADE, verbose_name='Договор', null=True)
 
+    class Meta: 
+        verbose_name = 'Тип недвижимости'
+        verbose_name_plural = 'Типы недвижимости'
+
 class Estate(models.Model):
     name = models.SmallIntegerField(verbose_name="Тип недвижимости")
     address = models.CharField(verbose_name="Адрес", max_length=150)
     cadastral_number = models.CharField(verbose_name="Кадастровый номер", max_length=50)
+    class Meta: 
+        verbose_name = 'Недвижимость'
+        verbose_name_plural = 'Недвижимости'
 
 
 class Agreement(models.Model):
     price = models.IntegerField(verbose_name="Стоимость")
+
+    class Meta: 
+        verbose_name = 'Договор'
+        verbose_name_plural = 'Договоры'
